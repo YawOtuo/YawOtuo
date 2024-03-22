f97f545b6f8104c39ae9a5b260aef9e1602c6edb
YawOtuo <yotuo2003@gmail.com>
2024-03-22 00:47:25 +0000

    welcome messages font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-start lg:items-center justify-center text-white h-[100vh] gap-4 px-5">
-        <h1 className="text-5xl font-bold">Hi! I am Yaw Otuo</h1>
+      <div className="flex flex-col items-start lg:items-center justify-center text-white h-[100vh] gap-4 lg:gap-2 px-5">
+        <h1 className="text-3xl font-bold">Hi! I am Yaw Otuo</h1>
 
-        <p className="text-xl ">
+        <p className="text-base lg:text-xl ">
           &quot;The most important skill for a programmer is the ability to
           effectively communicate ideas.&quot;
         </p>
