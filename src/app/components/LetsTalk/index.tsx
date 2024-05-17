c628bb7ab4a32adebeba5e057f140546b82159e8
YawOtuo <yotuo2003@gmail.com>
2024-05-17 20:00:59 +0000

    "Update LetsTalk component: modify grid layout and add margin"

--- src/app/components/LetsTalk/index.tsx
+++ src/app/components/LetsTalk/index.tsx
@@ -8,8 +8,10 @@ function LetsTalk() {
     <div className="wrapper py-20">
       <h1 className="title">Let&apos;s talk</h1>
 
-      <div className="grid grid-cols-2  gap-20">
+      <div className="grid grid-cols-2 mt-3 lg:mt-0 gap-7 lg:gap-20">
+
         <div className="col-span-2 lg:col-span-1">
+
           <LottieFileBuilder
             animationData={developer1Json}
             width={"100%"}
